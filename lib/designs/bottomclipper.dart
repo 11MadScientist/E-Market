a22f1290cf9aab38b83e7fff9e148fb49872adcc
Hououin_Kyouma <felnovert.lee@gmail.com>
2021-03-18 20:26:48 +0800

    designing clipPath

--- lib/designs/bottomclipper.dart
+++ lib/designs/bottomclipper.dart
@@ -1,22 +1,21 @@
 import 'package:flutter/material.dart';
 
-class MyClipper extends CustomClipper<Path>
+class BottomClip extends CustomClipper<Path>
 {
   @override
   Path getClip(Size size) {
     var path = new Path();
-    path.lineTo(0, size.height-size.height/4);
-    var firstControlPoint = Offset(size.width/4, size.height);
-    var firstEndPoint = Offset(size.width/2, size.height);
+    path.lineTo(0, size.height - size.height/4);
+    var firstControlPoint = Offset(size.width/4, size.height/4);
+    var firstEndPoint = Offset(size.width/2, size.height/4);
     path.quadraticBezierTo(firstControlPoint.dx, firstControlPoint.dy,
         firstEndPoint.dx, firstEndPoint.dy);
 
-    var secondControlPoint = Offset(size.width - size.width/4, size.height);
-    var secondEndPoint = Offset(size.width, size.height-size.height/4);
+    var secondControlPoint = Offset(size.width - size.width/4, size.height/4);
+    var secondEndPoint = Offset(size.width, size.height - size.height/4);
     path.quadraticBezierTo(secondControlPoint.dx, secondControlPoint.dy,
         secondEndPoint.dx, secondEndPoint.dy);
     path.lineTo(size.width, 0);
-
 
 
     return path;
@@ -27,5 +26,5 @@ class MyClipper extends CustomClipper<Path>
     // TODO: implement shouldReclip
     return true;
   }
-  
+
 }
--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -1,35 +1,166 @@
 import 'package:flutter/material.dart';
 import 'package:e_market/designs/myclipper.dart';
+import 'package:e_market/designs/bottomclipper.dart';
 
 class Login extends StatefulWidget {
   @override
   _LoginState createState() => _LoginState();
 }
-
 class _LoginState extends State<Login> {
+
+  TextEditingController _nameControler = TextEditingController();
+  bool secure = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children:<Widget>[
-          ClipPath(
-            clipper: MyClipper(),
-            child: Container(
-              height: 230,
-              decoration: BoxDecoration(
-                color: Colors.orange[500],
-              ),
-              child: Center(
-                child: Text("e-Market",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 35
-                ),),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children:<Widget>[
+            ClipPath(
+              clipper: MyClipper(),
+              child: Container(
+                height: 200,
+                decoration: BoxDecoration(
+                  color: Colors.orange[500],
+                ),
+                child: Center(
+                  child: Text("e-Merkado",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 35
+                  ),),
+                ),
               ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.fromLTRB(40,0,40,0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Login",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 7,),
+                  Text(
+                    "Please login to your account.",
+                    style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.normal,
+                    ),
+                  ),
+                  SizedBox(height: 30,),
+                  TextField(
+                    decoration: InputDecoration(
+                    hintText: "Email Address",
+                    enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.orange[500]),
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+
+                    focusedBorder: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.green),
+                    borderRadius: BorderRadius.all(Radius.circular(30)),
+                    ),
+                    suffixIcon: Icon(
+                    Icons.email,
+                    color: Colors.orange[500],
+                    ),
+                    ),
+                    keyboardType: TextInputType.emailAddress,
+                    maxLines: 1,
+                  ),
+                  SizedBox(height: 30,),
+                  TextField(
+                    decoration: InputDecoration(
+                      hintText: "Password",
+                      enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.orange[500]),
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                      ),
+
+                      focusedBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.green),
+                        borderRadius: BorderRadius.all(Radius.circular(30)),
+                      ),
+                      suffixIcon: IconButton(
+                        icon: Icon(
+                          secure ? Icons.security:Icons.remove_red_eye,
+                          color: Colors.orange[500],
+                        ),
+                        onPressed: ()
+                        {
+                          setState(() {
+                            secure = !secure;
+                          });
+                        },
+                      ),
+                    ),
+                    obscureText: secure,
+                    maxLines: 1,
+                  ),
+                  SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text(
+                        "Forgot Password?",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.orange[500],
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 20,),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account?",
+                         style: TextStyle(
+                           fontWeight: FontWeight.bold,
+                           fontSize: 14,
+                      ),),
+                      SizedBox(width: 4,),
+                      Text(
+                        "Create new now!",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                          decoration: TextDecoration.underline,
+                          decorationColor: Colors.orange[500],
+                          decorationThickness: 2,
+                          color: Colors.orange[500],
+                        ),),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 20,),
+            Container(
+              color: Colors.orange[500],
+              child: ClipPath(
+                clipper: BottomClip(),
+                child: Container(
+                  height: 150,
+                  decoration: BoxDecoration(
+                    color: Colors.grey[50],
+                  ),
+
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
