5bdfbfe29478db1c70a4ca64bd31204ec4391270
febieclinton <72293441+febieclinton@users.noreply.github.com>
2021-03-25 10:37:37 +0800

    updated with theming

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:e_market/pages/home.dart';
 
 void main() => runApp(MaterialApp(
+      // THEMING
+      theme: ThemeData(
+          primarySwatch: Colors.orange,
+          fontFamily: 'Poppins',
+          appBarTheme: AppBarTheme(
+            foregroundColor: Colors.white,
+            backwardsCompatibility: false,
+          ),
+          textTheme: TextTheme(
+            bodyText1: TextStyle(),
+            bodyText2: TextStyle(),
+          ).apply(
+            bodyColor: Colors.blue[900],
+            displayColor: Colors.white,
+          )),
       home: home(),
     ));
