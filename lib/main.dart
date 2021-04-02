ef72421d62d692a2506032f6ccdeeab310520b59
Hououin_Kyouma <felnovert.lee@gmail.com>
2021-04-02 19:31:54 +0800

    initial integration successful

--- lib/main.dart
+++ lib/main.dart
@@ -5,51 +5,51 @@ import 'package:e_market/pages/landing.dart';
 import 'package:e_market/pages/home.dart';
 import 'package:e_market/pages/user_profile.dart';
 
-// void main() => runApp(MaterialApp(
-//   initialRoute: '/',
-//   routes: {
-//     '/'       : (context) => Landing(),
-//     '/login'  :(context) => Login(),
-//     '/signup' :(context) => SignUp(),
-//     '/home'   :(context) => Home(),
-//     '/profile':(context) => UserProfile()
-//   },
-// ));
+void main() => runApp(MaterialApp(
+  initialRoute: '/',
+  routes: {
+    '/'       : (context) => Landing(),
+    '/login'  :(context) => Login(),
+    '/signup' :(context) => SignUp(),
+    '/home'   :(context) => Home(),
+    '/profile':(context) => UserProfile()
+  },
+));
 
-void main()
-{
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: MyStatelessWidget(),
-    );
-  }
-}
-
-class MyStatelessWidget extends StatelessWidget
-{
-  const MyStatelessWidget({Key key}) : super(key:key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Emarket'),
-      ),
-      body: Center(child: Text('Press the button below!'),),
-      floatingActionButton: FloatingActionButton(
-        onPressed: ()
-        {
-          Navigator.push(context, MaterialPageRoute(builder: (context)=>UserProfile()));
-        },
-        child: Icon(Icons.navigation),
-        backgroundColor: Colors.green,
-
-      ),
-    );
-  }
-}
+// void main()
+// {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: MyStatelessWidget(),
+//     );
+//   }
+// }
+//
+// class MyStatelessWidget extends StatelessWidget
+// {
+//   const MyStatelessWidget({Key key}) : super(key:key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Emarket'),
+//       ),
+//       body: Center(child: Text('Press the button below!'),),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: ()
+//         {
+//           Navigator.push(context, MaterialPageRoute(builder: (context)=>UserProfile()));
+//         },
+//         child: Icon(Icons.navigation),
+//         backgroundColor: Colors.green,
+//
+//       ),
+//     );
+//   }
+// }
