b190cbec6da3c2eb3cd43a7991f3c3af6e309e1e
Hououin_Kyouma <felnovert.lee@gmail.com>
2021-04-07 22:05:49 +0800

    fixing popup outside size responsiveness

--- lib/designs/popup.dart
+++ lib/designs/popup.dart
@@ -16,54 +16,56 @@ class PopUp{
   {
     return(
         showDialog(
-            context: context,
-            barrierDismissible: false,
-            builder: (BuildContext context){
-              return AlertDialog(
-                insetPadding: EdgeInsets.symmetric(vertical: 250, horizontal: 100),
-                title: Text(
-                  title,
-                  style: TextStyle(
-                    fontFamily: 'Poppins',
-                    fontWeight: FontWeight.bold,
-                    fontSize: data.size.width * .05,
-                  ),),
-                content: Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Icon(
-                        icon,
-                        color: Colors.red,
-                        size: data.size.width * .15 ,
-                      ),
-                      // Expanded(child: SizedBox(width: data.size.width * .005,)),
-                      Expanded(
-                        child: Text(
-                          message,
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontFamily: 'Poppins',
-                            fontSize: data.size.width * .05,
-                          ),),
-                      ),
-                    ],
-                  ),
-                ),
-                actions: <Widget>[
-                  TextButton(
-                      onPressed: () {
-                        Navigator.of(context).pop();
-                      },
+          context: context,
+          barrierDismissible: false,
+          builder: (BuildContext context){
+            return AlertDialog(
+              insetPadding: EdgeInsets.symmetric(
+                  vertical: data.size.height*.15,
+                  horizontal: data.size.width*.10),
+              title: Text(
+                title,
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontWeight: FontWeight.bold,
+                  fontSize: data.size.width * .05,
+                ),),
+              content: Container(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Icon(
+                      icon,
+                      color: Colors.red,
+                      size: data.size.width * .15,
+                    ),
+                    // Expanded(child: SizedBox(width: data.size.width * .005,)),
+                    Expanded(
                       child: Text(
-                        'Close',
+                        message,
+                        textAlign: TextAlign.center,
                         style: TextStyle(
                           fontFamily: 'Poppins',
-                          fontSize: data.size.width * .04,
-                        ),
-                      ))
-                ],
-              );
+                          fontSize: data.size.width * .05,
+                        ),),
+                    ),
+                  ],
+                ),
+              ),
+              actions: <Widget>[
+                TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: Text(
+                      'Close',
+                      style: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontSize: data.size.width * .04,
+                      ),
+                    ))
+              ],
+            );
             })
     );
   }
