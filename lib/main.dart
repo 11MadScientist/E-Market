ff23dca924cc10c9d43770f1022dfc4d555c3650
febieclinton <febieclintlapera@gmail.com>
2021-04-11 17:12:15 +0800

    updated to view product list directly

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,24 @@ import 'package:e_market/pages/signup.dart';
 import 'package:e_market/pages/landing.dart';
 import 'package:e_market/pages/home.dart';
 import 'package:e_market/pages/cart.dart';
+import 'package:e_market/pages/productList_seller.dart';
 
 void main() => runApp(MaterialApp(
-  initialRoute: '/cart',
-  routes: {
-    '/': (context) => Landing(),
-    '/login': (context) => Login(),
-    '/signup': (context) => SignUp(),
-    '/home': (context) => Home(),
-    '/cart': (context) => Cart(),
-  },
-));
+      // THEMING
+      theme: ThemeData(
+          scaffoldBackgroundColor: Colors.white,
+          primarySwatch: Colors.orange,
+          fontFamily: 'Poppins',
+          appBarTheme: AppBarTheme(
+            foregroundColor: Colors.white,
+            backwardsCompatibility: false,
+          ),
+          textTheme: TextTheme(
+            bodyText1: TextStyle(),
+            bodyText2: TextStyle(),
+          ).apply(
+            bodyColor: Colors.blue[900],
+            displayColor: Colors.blue[900],
+          )),
+      home: product(),
+    ));
